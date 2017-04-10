19b803dc0874eaa07739d1cb8ecb7a10e9272cdf
Alex Merkert <alexm@tech4imaging.com>
2017-04-10 17:07:19 -0400

    added footer and auto-scroll.js

---
+++ scripts/auto-scroll.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+	var hasScrolled = false;
+	window.setTimeout(function () {
+		document.body.onscroll = function () {
+			hasScrolled = true;
+		};
+	}, 100);
+	
+	window.setTimeout(function () {
+		if (!hasScrolled) {
+			easyScroll();
+		}
+	}, 15000);
+	
+	var scrollTarget = 0;
+	var scrollLeft = Math.min(600, window.innerHeight);
+	var easyScroll = function () {
+		scrollTarget += scrollLeft/10;
+		scrollLeft = 9*scrollLeft/10;
+		window.scrollTo(0, scrollTarget);
+		if (scrollLeft > 1) {
+			window.setTimeout(easyScroll, 24);
+		}
+	};
+})();
