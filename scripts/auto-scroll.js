29bb7911d7d825ea954ebdb021a11cab2c8debcf
Alex Merkert <alexm@tech4imaging.com>
2017-04-24 13:22:48 -0400

    -fixed weather on phones -added hidden menu to home page

--- scripts/auto-scroll.js
+++ scripts/auto-scroll.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var scrollDown;
+
 (function () {
 	var hasScrolled = false;
 	var autoScrolled = false;
@@ -11,22 +13,30 @@
 		};
 	}, 100);
 	
-	window.setTimeout(function () {
+	/*var timer = window.setTimeout(function () {
 		if (!hasScrolled && document.body.scrollTop == 0) {
 			easyScroll();
 		}
-	}, 15000);
+	}, 15000);*/
 	
 	var scrollTarget = 0;
-	var scrollLeft = Math.min(600, window.innerHeight);
+	var scrollRemains = Math.min(600, window.innerHeight);
 	var easyScroll = function () {
 		if (hasScrolled) return;
-		scrollTarget += scrollLeft/10;
-		scrollLeft = 9*scrollLeft/10;
+		scrollTarget += scrollRemains/10;
+		scrollRemains = 9*scrollRemains/10;
 		autoScrolled = true;
 		window.scrollTo(0, scrollTarget);
-		if (scrollLeft > 1) {
+		if (scrollRemains > 1) {
 			window.setTimeout(easyScroll, 24);
 		}
+	};
+	
+	scrollDown = function () {
+		//window.clearTimeout(timer);
+		hasScrolled = false;
+		scrollTarget = 0;
+		scrollRemains = Math.min(600, window.innerHeight);
+		easyScroll();
 	};
 })();
